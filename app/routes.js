d22a7660f58f26b439fdb7513109683d2998ad4a
Ariel Ivan Diaz <arielivandiaz@gamil.com>
2018-11-09 14:36:46 -0300

    asd

--- app/routes.js
+++ app/routes.js
@@ -1,87 +1,90 @@
 
-const cmd = require('./commands');
+const cmd = require('./delays');
+
+const connections = [];
+
+module.exports = function (app, io) {
+
+	/*
+		io.sockets.on('connection', (socket) => {
+			connections.push(socket);
+			console.log(' %s sockets is connected', connections.length);
+	
+			socket.on('disconnect', () => {
+				connections.splice(connections.indexOf(socket), 1);
+			});
+	
+			socket.on('sending message', (message) => {
+				console.log('Message is received :', message);
+	
+				io.sockets.emit('new message', { message: message });
+			});
+		});
+	*/
 
 
-module.exports = function (app) {
-
-	let log = "Log: ";
 	// Index
 	app.get('/', function (req, res) {
 
 		res.render('index.ejs', {
 			message: 'Hello Node World',
-			log: log
 		});
 	});
-
 
 	app.post('/', function (req, res) {
 
-		run(cmd.step_1).then((resolve) => {
-			console.log("stdout :\n", resolve);
-			log += ("< Folder Created >");
+
+		var log = "";
+		io.sockets.emit('new message', { message: "Started" });
+
+		cmd.function_step_1().then((resolve) => {
+
+			log = "First Step OK";
 			console.log(log);
-			run(cmd.step_2).then((resolve) => {
-				console.log("stdout :\n", resolve);
-				log += ( "< Inside new Folder  >");
+			io.sockets.emit('new message', { message: log });
+
+			cmd.function_step_2().then((resolve) => {
+
+				log = "Second Step OK";
 				console.log(log);
-				run(cmd.step_3).then((resolve) => {
-					console.log("stdout :\n", resolve);
-					log += ( "< New File Created >");
+				io.sockets.emit('new message', { message: log });
+
+				cmd.function_step_3().then((resolve) => {
+
+					log = "Final Step OK";
 					console.log(log);
-					run(cmd.step_4).then((resolve) => {
-						console.log("stdout :\n", resolve);
-						log += ( "< Backup File Created >" );
-						res.redirect('/success');
-			
-					}).catch(function (reject) {
-						console.log(reject);
-						log += ( '<' +  reject + ' > ');
-						res.redirect('/failed');
-					
-					});
-		
+					io.sockets.emit('new message', { message: log });
+
 				}).catch(function (reject) {
-					console.log(reject);
-					log += ( '<' +  reject + ' > ');
-					res.redirect('/failed');
-				
+
+					log = "Final Step FAIL";
+					console.log(log);
+					io.sockets.emit('new message', { message: log });
+
 				});
-	
+
 			}).catch(function (reject) {
-				console.log(reject);
-				log += ( '<' +  reject + ' > ');
-				res.redirect('/failed');
-			
+
+				log = "Second Step FAIL";
+				console.log(log);
+				io.sockets.emit('new message', { message: log });
+
 			});
 
 		}).catch(function (reject) {
-			console.log(reject);
-			log += ( ' < ' +  reject + ' > ');
-			res.redirect('/failed');
-		
-		});
 
-	});
+			log = "First Step FAIL";
+			console.log(log);
+			io.sockets.emit('new message', { message: log });
 
-
-	app.get('/success', function (req, res) {
-
-		res.render('index.ejs', {
-			message: 'SUCCESS',
-			log: log
-		});
-	});
-
-
-	app.get('/failed', function (req, res) {
-
-		res.render('index.ejs', {
-			message: 'FAILED',
-			log: log
 		});
 	});
 
 
 
 }
+
+
+/*
+			
+*/
--- server.js
+++ server.js
@@ -1,25 +1,20 @@
-
-// Requirements
-var express = require('express'); //Express main framework
 var morgan = require('morgan'); //Request logger
-
-
-var app = express(); 
+var express = require('express'); //Express main framework
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
 var port = process.env.PORT || 3838;
 
-
-// Front-end application is stored in /public (.js, .css, .ejs)
-// Remember ejs ~ html
 app.use(express.static(__dirname + '/public'));
-
 
 app.use(morgan('dev')); // Set morgan to log every request to the console
 
 app.set('view engine', 'ejs'); // Set up ejs how templates motor
 
-// Define routes ****************************************************************
-require('./app/routes.js')(app); // load our routes and pass in our app and fully configured passport
 
-// Run app ***********************************************************************
-app.listen(port);
-console.log('Visit http://localhost:' + port);
+require('./app/routes.js')(app,io); 
+
+server.listen(port);
+console.log('Server is running');
+
+
