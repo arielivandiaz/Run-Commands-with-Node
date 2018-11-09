501c1aff8fbdfb8997946839555118ea6b2cbcc2
Ariel Ivan Diaz <arielivandiaz@gamil.com>
2018-11-09 10:08:55 -0300

    run example

--- app/routes.js
+++ app/routes.js
@@ -1,18 +1,4 @@
 
-const { spawn } = require('child_process');
-const child = spawn('ls', ['-lh', '/usr']);
-
-// use child.stdout.setEncoding('utf8'); if you want text chunks
-child.stdout.on('data', (chunk) => {
-  // data from standard output is here as buffers
-});
-
-// since these are streams, you can pipe them elsewhere
-child.stderr.pipe(dest);
-
-child.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
 
 module.exports = function (app) {
 	
